15e3c5ebdcdf80e6fff8ffcadcaa734f358e1c27
Cristian Campos Agüero <ccampos04@gmail.com>
2023-04-26 13:49:49 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,53 +1,11 @@
-import Laboratorio7;
+import encriptar;
+import desencriptar;
 import pytest;
 
-matriz = [[2,4,6,8,10], [1,3,5,7,9], [4,8,12,16,20], [0,0,0,0,0], [5,10,5,10,5]]
+texto = "CASA"
 
-def test_cortarMatriz_1():
-    assert Laboratorio7.cortarMatriz(matriz, 2, 3) == [[2,4,6], [1,3,5]]
+def test_encriptar_1():
+    assert encriptar.encriptar(texto) == "043041053041"
     
-def test_cortarMatriz_2():
-    assert Laboratorio7.cortarMatriz(matriz, 2, 2) == [[2,4], [1,3]]
-    
-def test_cortarMatriz_3():
-    assert Laboratorio7.cortarMatriz(matriz, 5, 5) == [[2,4,6,8,10], [1,3,5,7,9], [4,8,12,16,20], [0,0,0,0,0], [5,10,5,10,5]]
-    
-def test_cortarMatriz_4():
-    assert  isinstance( str(Laboratorio7.cortarMatriz(matriz, 8, 5)), str) == isinstance('Error: los valores de la nueva matriz exceden las dimensiones actuales', str)
-    
-###########################################################################################    
-matriz2 = [[2,4,6,8,10], [1,3,5,7,9], [4,8,12,16,20], [10,20,30,40,50], [5,10,15,20,25]]
-
-def test_cortarMatrizv2_1():
-    assert Laboratorio7.cortarMatriz_v2(matriz2, 2, 3, 2, 2) == [ [16,20], [40,50] ]
-    
-def test_cortarMatrizv2_2():
-    assert Laboratorio7.cortarMatriz_v2(matriz2, 2, 0, 2, 2) == [ [4,8], [10,20] ]
-    
-def test_cortarMatrizv2_3():
-    assert Laboratorio7.cortarMatriz_v2(matriz2, 0,0,5,5) == [[2,4,6,8,10], [1,3,5,7,9], [4,8,12,16,20], [10,20,30,40,50], [5,10,15,20,25]]
-    
-def test_cortarMatrizv2_4():
-    assert  isinstance( str(Laboratorio7.cortarMatriz_v2(matriz2, 2, 1, 6, 6)), str) == isinstance('Error: los valores de la nueva matriz exceden las dimensiones actuales', str)
-
-
-###########################################################################################                       
-matriz3 = [ [1,0,0,0,0], [1,1,1,1,1], [0,1,0,1,0]]
-matriz4 = [ [1,0], [0,0], [1,1], [1,0], [0,1], [1,1]]
-
-def test_convertirBinADec_1():
-    assert Laboratorio7.convertirBinADec(matriz3) == [16, 31, 10]
-                       
-def test_convertirBinADec_2():
-    assert Laboratorio7.convertirBinADec(matriz4) == [2,0,3,2,1,3]
-                       
-
-###########################################################################################                       
-matriz5 = [ [1,0,0,0,0], [1,1,1,1,1], [0,1,0,1,0]]
-matriz6 = [ ['A',1,0], [0,'B',0], ['F','F',1], ['A','B',5], [2,'C',9]]
-
-def test_convertirHexADec_1():
-    assert Laboratorio7.convertirHexADec(matriz5) == [65536, 69905, 4112]
-                       
-def test_convertirHexADec_2():
-    assert Laboratorio7.convertirHexADec(matriz6) == [2576, 176, 4081, 2741, 713]                       
+def test_desencriptar_1():
+    assert desencriptar.desencriptar("043041053041") == texto                      
